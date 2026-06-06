0819fe7a298d65388779006697c0187a3ad41ece
diazghyfary-boop <diazghyfary@gmail.com>
2026-06-06 20:22:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,8 @@ if fitur == "Menghitung Standarisasi Larutan":
             if volume > 0:
                 BE = 0.063  # mg/mgrek (BM=126, valensi=2 → BE=63 g/ek = 0.063 mg/mgrek)
                 N = massa / ((100 / 25) * volume * BE)
-                st.success(f"Normalitas NaOH = {N:.4f} N")
+                st.success(f"Normalitas NaOH = mg Asam Oksalat / ((100 mL/25 mL) × mL NaOH × BE Asam Oksalat)
+                {N:.4f} N")
             else:
                 st.error("Volume tidak boleh 0.")
 
